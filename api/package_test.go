15bab271825be2fda3316648e5016cdcc5119aeb
Wiliam Souza <wiliamsouza83@gmail.com>
2013-11-20 20:32:36 -0200

    Added tests and code to response body in packageList

--- api/package_test.go
+++ api/package_test.go
@@ -8,7 +8,20 @@ import (
 )
 
 func TestPackageList(t *testing.T) {
-	request, _ := http.NewRequest("GET", "test/package", nil)
+	results := `[{"_id" : ObjectId("5287a2b1e5adc4f8e90b50a2"),
+	           "filename" : "pkg-1.0.tar.gz",
+		   "chunkSize" : 262144,
+		   "uploadDate" : ISODate("2013-11-16T16:52:01.089Z"),
+		   "md5" : "ac98f052e55d0c556e6536796d6b53eb",
+		   "length" : 2133},
+	           {"_id" : ObjectId("5287a2b1e5adc4f8e90b50a3"),
+	           "filename" : "pkg-2.0.tar.gz",
+		   "chunkSize" : 262145,
+		   "uploadDate" : ISODate("2013-12-16T16:52:01.089Z"),
+		   "md5" : "ac98f052e55d0c556e6536796d6b54eb",
+		   "length" : 2134}]`
+
+	request, _ := http.NewRequest("GET", "tests/packages", nil)
 	response := httptest.NewRecorder()
 
 	packageList(response, request)
@@ -21,10 +34,14 @@ func TestPackageList(t *testing.T) {
 	if !strings.EqualFold(ct, "application/json") {
 		t.Fatalf("Content-Type does not equal 'application/json'")
 	}
+
+	if !strings.EqualFold(response.Body.String(), results) {
+		t.Fatalf("Body does not equal 'results'")
+	}
 }
 
 func TestPackageUpload(t *testing.T) {
-	request, _ := http.NewRequest("POST", "test/package", nil)
+	request, _ := http.NewRequest("POST", "tests/packages", nil)
 	response := httptest.NewRecorder()
 
 	packageUpload(response, request)
@@ -40,7 +57,7 @@ func TestPackageUpload(t *testing.T) {
 }
 
 func TestPackageDownload(t *testing.T) {
-	request, _ := http.NewRequest("GET", "test/package/pkg.tgz", nil)
+	request, _ := http.NewRequest("GET", "tests/packages/pkg.tgz", nil)
 	response := httptest.NewRecorder()
 
 	packageDownload(response, request)
