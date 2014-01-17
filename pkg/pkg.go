f82bb233a610e8039662aeb0f0c3ca4af5f2fe7b
Wiliam Souza <wiliamsouza83@gmail.com>
2014-01-16 22:59:16 -0200

    Added ListPackges function and Package, PackageList types

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,1 +1,37 @@
 package pkg
+
+import (
+	"github.com/wiliamsouza/apollo/db"
+	"encoding/json"
+	"strings"
+)
+
+type Package struct {
+	Name string
+	Description string
+}
+
+func (pkg *Package) String() string {
+        parts := make([]string, 1, 2)
+        if pkg.Description != "" {
+                parts = append(parts, pkg.Description)
+        }
+        return strings.Join(parts, " ")
+}
+
+
+type PackageList []Package
+
+func (packages PackageList) MarshalJSON() ([]byte, error) {
+        m := make(map[string]string, len(packages))
+        for _, pkg := range packages {
+                m[pkg.Name] = pkg.String()
+        }
+        return json.Marshal(m)
+}
+
+func ListPackages() (PackageList, error) {
+	var packages []Package
+	err := db.Session.Package().Find(nil).Sort("_id").All(&packages)
+	return PackageList(packages), err
+}
--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -1,1 +1,34 @@
 package pkg
+
+import (
+	"github.com/wiliamsouza/apollo/db"
+	"github.com/globocom/config"
+	"launchpad.net/gocheck"
+	"testing"
+)
+
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type S struct{}
+
+var _ = gocheck.Suite(&S{})
+
+func (s *S) SetUpSuite(c *gocheck.C) {
+        err := config.ReadConfigFile("../etc/apollo.conf")
+        c.Check(err, gocheck.IsNil)
+        config.Set("database:name", "apollo_pkg_tests")
+        db.Connect()
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+        db.Session.DB.DropDatabase()
+}
+
+//func (s *S) TestListPackagesShouldReturnAPackageList(c *gocheck.C) {
+//	var pkg Package
+//        pkg = &Package{Name: "bluetooth.tgz", Description: "Bluetooth test"}
+//	var pkglist PackageList
+//	pkglist = PackageList(pkg)
+//	pkgList := ListPackages()
+//	c.Assert(pkglist, gocheck.DeepEquals, pkgList)
+//}
