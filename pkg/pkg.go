0ce63124753663cb61bffbf983bba56b2ae59544
Wiliam Souza <wiliamsouza83@gmail.com>
2014-01-18 02:39:44 -0200

    Added NewPackage tests and done some refactory

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,71 +1,55 @@
 package pkg
 
 import (
-	"github.com/wiliamsouza/apollo/db"
 	"encoding/json"
-	"mime/multipart"
-	"strings"
+	"github.com/wiliamsouza/apollo/db"
 	"io"
 	"io/ioutil"
+	"mime/multipart"
+	"strings"
 )
 
 type Package struct {
-	Name string
-	Description string
+	Filename    string `json:"filename"`
+	Description string `json:"description"`
 }
 
 func (pkg *Package) String() string {
-        parts := make([]string, 1, 2)
-        if pkg.Description != "" {
-                parts = append(parts, pkg.Description)
-        }
-        return strings.Join(parts, " ")
+	parts := make([]string, 1, 2)
+	if pkg.Description != "" {
+		parts = append(parts, pkg.Description)
+	}
+	return strings.Join(parts, " ")
 }
 
 type PackageList []Package
 
 func (packages PackageList) MarshalJSON() ([]byte, error) {
-        m := make(map[string]string, len(packages))
-        for _, pkg := range packages {
-                m[pkg.Name] = pkg.String()
-        }
-        return json.Marshal(m)
+	m := make(map[string]string, len(packages))
+	for _, pkg := range packages {
+		m[pkg.Filename] = pkg.String()
+	}
+	return json.Marshal(m)
 }
 
 type Metadata struct {
-	Name string `json:"name"`
-	Version float32 `json:"version"`
-	Description string `json:"description"`
-	Install string `json:"install"`
-	Run string `json:"run"`
+	Version     float32 `json:"version"`
+	Description string  `json:"description"`
+	Install     string  `json:"install"`
+	Run         string  `json:"run"`
 }
 
-func NewPackage(file, meta, multipart.File, filename string) (Package, error) {
-	gfs, err : db.Session.Package().Create(filename)
-
-	err = io.Copy(gfs, file)
-	if err != nil {
-		return nil, err
-	}
-
+func NewPackage(file, meta multipart.File, filename string) (Package, error) {
+	pkg := Package{}
+	gfs, err := db.Session.Package().Create(filename)
+	_, err = io.Copy(gfs, file)
 	data, err := ioutil.ReadAll(meta)
-	if err != nil {
-		return nil, err
-	}
-
-	metadata := &Metadata{}
+	metadata := Metadata{}
 	json.Unmarshal(data, &metadata)
 	gfs.SetMeta(metadata)
-
-	pkg := Package{}
-	pkg.Name = filename
+	pkg.Filename = filename
 	pkg.Description = metadata.Description
-
 	err = gfs.Close()
-	if err != nil {
-		return nil, err
-	}
-
 	return pkg, err
 }
 
--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -1,19 +1,13 @@
 package pkg
 
 import (
-	"github.com/wiliamsouza/apollo/db"
 	"github.com/globocom/config"
+	"github.com/wiliamsouza/apollo/db"
+	"labix.org/v2/mgo/bson"
 	"launchpad.net/gocheck"
+	"os"
 	"testing"
 )
-
-setup := `{
-	"name": "bluetooth"
-	"version": 0.1
-	"description": "Bluetooth ON/Off test"
-	"install": "adb push dist/bluetooth.jar /data/local/tmp/"
-	"run": "adb shell uiautomator runtest bluetooth.jar -c com.github.wiliamsouza.bluetooth.BluetoothTest"}`
-
 
 func Test(t *testing.T) { gocheck.TestingT(t) }
 
@@ -22,20 +16,30 @@ type S struct{}
 var _ = gocheck.Suite(&S{})
 
 func (s *S) SetUpSuite(c *gocheck.C) {
-        err := config.ReadConfigFile("../etc/apollo.conf")
-        c.Check(err, gocheck.IsNil)
-        config.Set("database:name", "apollo_pkg_tests")
-        db.Connect()
+	err := config.ReadConfigFile("../etc/apollo.conf")
+	c.Check(err, gocheck.IsNil)
+	config.Set("database:name", "apollo_pkg_tests")
+	db.Connect()
 }
 
 func (s *S) TearDownSuite(c *gocheck.C) {
-        db.Session.DB.DropDatabase()
+	db.Session.DB.DropDatabase()
+}
+
+func (s *S) TestNewPackage(c *gocheck.C) {
+	filename := "bluetooth.tgz"
+	pkgFile, _ := os.Open("../data/" + filename)
+	metaFile, _ := os.Open("../data/metadata.json")
+	pkg, _ := NewPackage(pkgFile, metaFile, filename)
+	var pkgDb Package
+	_ = db.Session.Package().Files.Find(bson.M{"filename": filename}).Select(bson.M{"filename": 1, "metadata.description": 1}).One(&pkgDb)
+	c.Assert(pkgDb, gocheck.DeepEquals, pkg)
 }
 
 /*
 func (s *S) TestListPackagesShouldReturnAPackageList(c *gocheck.C) {
 	var pkg Package
-        pkg = &Package{Name: "bluetooth.tgz", Description: "Bluetooth test"}
+        pkg = &Package{Filename: "bluetooth.tgz", Description: "Bluetooth test"}
 	var pkglist PackageList
 	pkglist = PackageList(pkg)
 	pkgList := ListPackages()
