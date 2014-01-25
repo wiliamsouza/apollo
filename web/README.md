fad5b5e1fe848996c815ab3120ca81c3f1646072
Wiliam Souza <wiliamsouza83@gmail.com>
2014-01-24 22:28:01 -0200

    Added javascript client test code

---
+++ web/README.md
@@ -0,0 +1,26 @@
+Web client
+==========
+
+Test using [react](http://facebook.github.io/react/index.html)
+
+Usage
+-----
+
+```
+npm install express
+node server.js
+```
+
+Start `apollo-webserver` before:
+
+``` 
+../apollo-webserver -config ../etc/apollo.conf &
+```
+
+Start `express` server:
+
+```
+node server.js
+```
+
+Point your browser to http://localhost:3000/.
---
+++ web/scripts/apollo.js
@@ -0,0 +1,43 @@
+/** @jsx React.DOM */
+
+var NewUserForm = React.createClass({
+  handleSubmit: function() {
+    var name = this.refs.name.getDOMNode().value.trim();
+    var email = this.refs.email.getDOMNode().value.trim();
+    var pwd = this.refs.pwd.getDOMNode().value.trim();
+    var pass = this.refs.pass.getDOMNode().value.trim();
+    // TODO: Check password is the same here!
+    this.postUser({name: name, email: email, password:pass});
+    this.refs.name.getDOMNode().value = '';
+    this.refs.email.getDOMNode().value = '';
+    this.refs.pwd.getDOMNode().value = '';
+    this.refs.pass.getDOMNode().value = '';
+    return false;
+  },
+  postUser: function(user) {
+    $.ajax({
+      url: this.props.url,
+      type: 'POST',
+      data: user,
+      success: function(data) {
+	      console.log(data)
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <form className="newUserForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Full name." ref="name" />
+        <input type="text" placeholder="Your email." ref="email" />
+        <input type="password" placeholder="Password." ref="pwd" />
+        <input type="password" placeholder="Password again." ref="pass" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+React.renderComponent(
+  <NewUserForm url="http://localhost:8000/user" />,
+  document.getElementById('newUserForm')
+);
