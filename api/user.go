b6123b3a69bbd764d21bfe36a45239b0708d540a
Wiliam Souza <wiliamsouza83@gmail.com>
2014-01-25 01:20:58 -0200

    Added return http.Error and moved header status set to the end of new user function

--- api/user.go
+++ api/user.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -21,24 +22,28 @@ type responseUser struct {
 
 func NewUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Error parssing request body: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 	var u requestUser
 	err = json.Unmarshal(b, &u)
 	if err != nil {
 		http.Error(w, "Error parssing json request: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 	newUser, err := customer.NewUser(u.Name, u.Email, u.Password)
 	if err != nil {
 		http.Error(w, "Error creating new user: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 	user := responseUser{Name: newUser.Name, Email: newUser.Email}
 	result, err := json.Marshal(user)
 	if err != nil {
 		http.Error(w, "Error generating json result: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
+	w.WriteHeader(http.StatusCreated)
 	w.Write(result)
 }
