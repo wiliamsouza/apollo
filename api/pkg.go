ab929b9d7b4897da33a94c51467d99086e29e375
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-01 13:49:53 -0200

    Moved http response header set to the end of endpoint func

--- api/pkg.go
+++ api/pkg.go
@@ -12,7 +12,6 @@ import (
 
 func ListPackages(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	packages, err := pkg.ListPackages()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
@@ -23,12 +22,12 @@ func ListPackages(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 	w.Write(result)
 }
 
 func UploadPackage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 	pkgFile, pkgHeader, err := r.FormFile("package")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -49,12 +48,12 @@ func UploadPackage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusCreated)
 	w.Write(result)
 }
 
 func DetailPackage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	filename := filepath.Base(r.URL.Path)
 	pkg, err := pkg.DetailPackage(filename)
 	if err != nil {
@@ -66,11 +65,11 @@ func DetailPackage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 	w.Write(result)
 }
 
 func DownloadPackage(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	filename := filepath.Base(r.URL.Path)
 	pkg, err := pkg.GetPackage(filename)
 	if err != nil {
@@ -81,6 +80,7 @@ func DownloadPackage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("Content-Length", strconv.FormatInt(pkg.Size(), 10))
 	w.Header().Set("ETag", pkg.MD5())
+	w.WriteHeader(http.StatusOK)
 	io.Copy(w, pkg)
 
 }
