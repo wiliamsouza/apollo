f117e42c04fcab5162f8ce6d4869b2e4e15963e0
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-09 03:32:42 -0200

    Added organization New, Detail, List, Modify and Remove funcs and tests

--- customer/organization.go
+++ customer/organization.go
@@ -1,7 +1,57 @@
 package customer
 
+import (
+	"fmt"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
 type Organization struct {
-	Name   string
-	Teams  []string
-	Admins []string
+	Name   string   `json:"name" bson:"_id"`
+	Teams  []Team   `json:"teams" bson:"teams"`
+	Admins []string `json:"admins" bson:"admins"`
+}
+
+type Team struct {
+	Name  string
+	Users []string
+}
+
+type OrganizationList []Organization
+
+func NewOrganization(organization Organization) (Organization, error) {
+	if err := db.Session.Organization().Insert(&organization); err != nil {
+		return organization, err
+	}
+	return organization, nil
+}
+
+func ListOrganizations() (OrganizationList, error) {
+	var organizations []Organization
+	err := db.Session.Organization().Find(nil).Sort("_id").All(&organizations)
+	return OrganizationList(organizations), err
+
+}
+
+func DetailOrganization(name string) (Organization, error) {
+	var organization Organization
+	err := db.Session.Organization().FindId(name).One(&organization)
+	return organization, err
+
+}
+
+func ModifyOrganization(name string, organization Organization) error {
+	err := db.Session.Organization().UpdateId(name, organization)
+	if err != nil {
+		return fmt.Errorf("Error updating organization: %s", err.Error())
+	}
+	return nil
+}
+
+func RemoveOrganization(name string) error {
+	err := db.Session.Organization().RemoveId(name)
+	if err != nil {
+		return fmt.Errorf("Error removing organization: %s", err.Error())
+	}
+	return nil
 }
--- customer/organization_test.go
+++ customer/organization_test.go
@@ -1,1 +1,92 @@
 package customer
+
+import (
+	"launchpad.net/gocheck"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
+func (s *S) TestNewOrganization(c *gocheck.C) {
+	jhon := "jhon@doe.com"
+	jane := "jane@doe.com"
+	id := "doegroup"
+	t := Team{Name: "Test", Users: []string{jhon, jane}}
+	o := Organization{Name: id, Teams: []Team{t}, Admins: []string{jhon}}
+	org, err := NewOrganization(o)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Organization().RemoveId(id)
+	var orgDb Organization
+	err = db.Session.Organization().FindId(id).One(&orgDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(orgDb, gocheck.DeepEquals, org)
+}
+
+func (s *S) TestListOrganizations(c *gocheck.C) {
+	jhon := "jhon@doe.com"
+	jane := "jane@doe.com"
+	id1 := "jhoncorp"
+	id2 := "janecorp"
+	t := Team{Name: "Test", Users: []string{jhon, jane}}
+	o1 := Organization{Name: id1, Teams: []Team{t}, Admins: []string{jhon}}
+	o2 := Organization{Name: id2, Teams: []Team{t}, Admins: []string{jane}}
+	_, err := NewOrganization(o1)
+	c.Assert(err, gocheck.IsNil)
+	_, err = NewOrganization(o2)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Organization().RemoveId(id1)
+	defer db.Session.Organization().RemoveId(id2)
+	// BUG: The order here is relevant cause .Sort("_id")
+	orgList := OrganizationList{o2, o1}
+	orgListDb, err := ListOrganizations()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(orgListDb, gocheck.DeepEquals, orgList)
+}
+
+func (s *S) TestDetailOrganization(c *gocheck.C) {
+	jhon := "jhon@doe.com"
+	jane := "jane@doe.com"
+	id := "doegroup"
+	t := Team{Name: "Test", Users: []string{jhon, jane}}
+	o := Organization{Name: id, Teams: []Team{t}, Admins: []string{jhon}}
+	org, err := NewOrganization(o)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Organization().RemoveId(id)
+	orgDb, err := DetailOrganization(id)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(orgDb, gocheck.DeepEquals, org)
+}
+
+func (s *S) TestModifyOrganization(c *gocheck.C) {
+	jhon := "jhon@doe.com"
+	jane := "jane@doe.com"
+	id := "jhoncorp"
+	t1 := Team{Name: "Test", Users: []string{jhon}}
+	t2 := Team{Name: "Test", Users: []string{jhon, jane}}
+	o1 := Organization{Name: id, Teams: []Team{t1}, Admins: []string{jhon}}
+	o2 := Organization{Name: id, Teams: []Team{t2}, Admins: []string{jane}}
+	_, err := NewOrganization(o1)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Organization().RemoveId(id)
+	err = ModifyOrganization(id, o2)
+	c.Assert(err, gocheck.IsNil)
+	var orgDb Organization
+	err = db.Session.Organization().FindId(id).One(&orgDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(orgDb, gocheck.DeepEquals, o2)
+}
+
+func (s *S) TestRemoveOrganization(c *gocheck.C) {
+	jhon := "jhon@doe.com"
+	jane := "jane@doe.com"
+	id := "doegroup"
+	t := Team{Name: "Test", Users: []string{jhon, jane}}
+	o := Organization{Name: id, Teams: []Team{t}, Admins: []string{jhon}}
+	org, err := NewOrganization(o)
+	c.Assert(err, gocheck.IsNil)
+	err = RemoveOrganization(org.Name)
+	c.Assert(err, gocheck.IsNil)
+	lenght, err := db.Session.Organization().FindId(org.Name).Count()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(lenght, gocheck.Equals, 0)
+
+}
