7d65492e8daea678a109cf571996b6c27bd16f6e
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-09 15:14:33 -0200

    Added organizations handlers to apollo-webserver

--- bin/apollo-webserver.go
+++ bin/apollo-webserver.go
@@ -15,6 +15,13 @@ import (
 
 const version = "0.0.1"
 
+type MuxHandler func(http.ResponseWriter, *http.Request, map[string]string)
+
+func (h MuxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	h(w, r, vars)
+}
+
 func main() {
 	configFile := flag.String("config", "/etc/apollo.conf", "Apollo webserver configuration file")
 	gVersion := flag.Bool("version", false, "Print version and exit")
@@ -39,6 +46,12 @@ func main() {
 	r.HandleFunc("/tests/packages/{filename}", api.DetailPackage).Methods("GET")
 	r.HandleFunc("/tests/packages/downloads/{filename}", api.DownloadPackage).Methods("GET")
 	r.HandleFunc("/users", api.NewUser).Methods("POST")
+	r.HandleFunc("/organizations", api.NewOrganization).Methods("POST")
+	r.HandleFunc("/organizations", api.ListOrganizations).Methods("GET")
+	r.Handle("/organizations/{name}", MuxHandler(api.DetailOrganization)).Methods("GET")
+	r.Handle("/organizations/{name}", MuxHandler(api.ModifyOrganization)).Methods("PUT")
+	r.Handle("/organizations/{name}", MuxHandler(api.DeleteOrganization)).Methods("DELETE")
+
 	http.Handle("/", r)
 
 	bind, err := config.GetString("webserver:bind")
