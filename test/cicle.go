0b17d393c1c77cfb75386e8d526cd39966337a3f
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-25 00:25:40 -0300

    Added test.cicle new, list detail modify remove funcs and tests

--- test/cicle.go
+++ test/cicle.go
@@ -1,1 +1,72 @@
 package test
+
+import (
+	"errors"
+	"fmt"
+
+	"labix.org/v2/mgo/bson"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
+type Cicle struct {
+	Id       bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name     string        `json:"name" bson:"name"`
+	Device   string        `json:"device" bson:"device"`
+	Packages []string      `json:"packages" bson:""packages`
+}
+
+type CicleList []Cicle
+
+// NewCicle create new cicle
+func NewCicle(cicle Cicle) (Cicle, error) {
+	cicle.Id = bson.NewObjectId()
+	if err := db.Session.Cicle().Insert(&cicle); err != nil {
+		return cicle, err
+	}
+	return cicle, nil
+}
+
+// ListCicles list cicles
+func ListCicles() (CicleList, error) {
+	var cicles []Cicle
+	err := db.Session.Cicle().Find(nil).Sort("_id").All(&cicles)
+	return CicleList(cicles), err
+}
+
+// DetailCicle detail cicle
+func DetailCicle(objIdHex string) (Cicle, error) {
+	if !bson.IsObjectIdHex(objIdHex) {
+		return Cicle{}, errors.New("Invalid cicle object id hex")
+	}
+	id := bson.ObjectIdHex(objIdHex)
+	var cicle Cicle
+	err := db.Session.Cicle().FindId(id).One(&cicle)
+	return cicle, err
+}
+
+// ModifyCicle modify cicle
+func ModifyCicle(objIdHex string, cicle Cicle) error {
+	if !bson.IsObjectIdHex(objIdHex) {
+		return fmt.Errorf("Invalid cicle object id hex")
+	}
+	id := bson.ObjectIdHex(objIdHex)
+	err := db.Session.Cicle().UpdateId(id, cicle)
+	if err != nil {
+		return fmt.Errorf("error updating cicle: %s", err.Error())
+	}
+	return nil
+}
+
+// RemoveCicle remove cicle
+func RemoveCicle(objIdHex string) error {
+	if !bson.IsObjectIdHex(objIdHex) {
+		return fmt.Errorf("Invalid cicle object id hex")
+	}
+	id := bson.ObjectIdHex(objIdHex)
+	err := db.Session.Cicle().RemoveId(id)
+	if err != nil {
+		return fmt.Errorf("error removing cicle: %s", err.Error())
+	}
+	return nil
+}
--- test/cicle_test.go
+++ test/cicle_test.go
@@ -1,1 +1,95 @@
 package test
+
+import (
+	"testing"
+
+	"github.com/globocom/config"
+	"launchpad.net/gocheck"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type S struct{}
+
+var _ = gocheck.Suite(&S{})
+
+func (s *S) SetUpSuite(c *gocheck.C) {
+	err := config.ReadConfigFile("../etc/apollod.conf")
+	c.Check(err, gocheck.IsNil)
+	config.Set("database:name", "apollo_test_tests")
+	db.Connect()
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+	db.Session.DB.DropDatabase()
+}
+
+func (s *S) TestNewCicle(c *gocheck.C) {
+	cicle := Cicle{Name: "Test maguro", Device: "maguro",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	test, err := NewCicle(cicle)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Cicle().RemoveId(test.Id)
+	var cicleDb Cicle
+	err = db.Session.Cicle().FindId(test.Id).One(&cicleDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(cicleDb, gocheck.DeepEquals, test)
+}
+
+func (s *S) TestListCicles(c *gocheck.C) {
+	cicle1 := Cicle{Name: "Test maguro", Device: "maguro",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	cicle2 := Cicle{Name: "Test yakju", Device: "yakju",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	test1, err := NewCicle(cicle1)
+	c.Assert(err, gocheck.IsNil)
+	test2, err := NewCicle(cicle2)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Cicle().RemoveId(test1.Id)
+	defer db.Session.Cicle().RemoveId(test2.Id)
+	cicleList := CicleList{test1, test2}
+	cicleListDb, err := ListCicles()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(cicleListDb, gocheck.DeepEquals, cicleList)
+}
+
+func (s *S) TestDetailCicle(c *gocheck.C) {
+	cicle := Cicle{Name: "Test maguro", Device: "maguro",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	test, err := NewCicle(cicle)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Cicle().RemoveId(test.Id)
+	cicleDb, err := DetailCicle(test.Id.Hex())
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(cicleDb, gocheck.DeepEquals, test)
+}
+
+func (s *S) TestmodifyCicle(c *gocheck.C) {
+	cicle1 := Cicle{Name: "Test maguro", Device: "maguro",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	test, err := NewCicle(cicle1)
+	cicle2 := Cicle{Id: test.Id, Name: "Test yakju", Device: "yakju",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Cicle().RemoveId(test.Id)
+	err = ModifyCicle(test.Id.Hex(), cicle2)
+	c.Assert(err, gocheck.IsNil)
+	var cicleDb Cicle
+	err = db.Session.Cicle().FindId(test.Id).One(&cicleDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(cicleDb, gocheck.DeepEquals, cicle2)
+}
+
+func (s *S) TestRemoveCicle(c *gocheck.C) {
+	cicle := Cicle{Name: "Test maguro", Device: "maguro",
+		Packages: []string{"bluetooth.jar", "wifi.jar"}}
+	test, err := NewCicle(cicle)
+	c.Assert(err, gocheck.IsNil)
+	err = RemoveCicle(test.Id.Hex())
+	c.Assert(err, gocheck.IsNil)
+	lenght, err := db.Session.Cicle().FindId(test.Id).Count()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(lenght, gocheck.Equals, 0)
+}
