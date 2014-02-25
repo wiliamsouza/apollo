3903a663faa7da821dfdbd53a4f720941a0703c7
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-24 21:06:59 -0300

    Added new, detail, remove, update, list device func and tests

--- device/device.go
+++ device/device.go
@@ -1,43 +1,91 @@
 package device
 
+import (
+	"fmt"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
 // Device holds info about mobile devices
 type Device struct {
-	Permission      Permissions
-	Owner           string
-	Status          string
-	Name            string
-	Codename        string
-	Vendor          string
-	Manufacturer    string
-	Type            string
-	Platform        string
-	Cpu             string
-	Gpu             string
-	Ram             string
-	Weight          string
-	Dimensions      string
-	ScreenDimension string
-	Resolution      string
-	ScreenDensity   string
-	InternalStorage string
-	SdCard          string
-	Bluetooth       string
-	WiFi            string
-	MainCamera      string
-	SecondaryCamera string
-	Power           string
-	Peripherals     string
+	Codename        string      `json:"codename" bson:"_id"`
+	Permission      Permissions `json:"permission" bson:"permission"`
+	Owner           string      `json:"owner" bson:"owner"`
+	Status          string      `json:"status" bson:"status"`
+	Name            string      `json:"name" bson:"name"`
+	Vendor          string      `json:"vendor" bson:"vendor"`
+	Manufacturer    string      `json:"manufacturer" bson:"manufacturer"`
+	Type            string      `json:"type" bson:"type"`
+	Platform        string      `json:"platform" bson:"platform"`
+	Cpu             string      `json:"cpu" bson:"cpu"`
+	Gpu             string      `json:"gpu" bson:"gpu"`
+	Ram             string      `json:"ram" bson:"ram"`
+	Weight          string      `json:"weight" bson:"weight"`
+	Dimensions      string      `json:"dimensions" bson:"dimentions"`
+	ScreenDimension string      `json:"screenDimenion" bson:"screenDimention"`
+	Resolution      string      `json:"resolution" bson:"resolution"`
+	ScreenDensity   string      `json:"screenDensity" bson:"screenDensity"`
+	InternalStorage string      `json:"internalStorage" bson:"internalStorage"`
+	SdCard          string      `json:"sdCard" bson:"sdCard"`
+	Bluetooth       string      `json:"bluetooth" bson:"bluetooth"`
+	WiFi            string      `json:"wifi" bson:"wifi"`
+	MainCamera      string      `json:"mainCamera" bson:"mainCamera"`
+	SecondaryCamera string      `json:"secondaryCamera" bson:"secondaryCamera"`
+	Power           string      `json:"power" bson:"power"`
+	Peripherals     string      `json:"peripherals" bson:"peripherals"`
 }
+
+type DeviceList []Device
 
 // Permissions holds devices permission to organization and team
 type Permissions struct {
-	Organization Permission
-	Team         Permission
+	Organization Permission `json:"organization" bson:"organization"`
+	Team         Permission `json:"team" bson:"team"`
 }
 
 // Permission holds what type of permission a organization and team have
 type Permission struct {
-	Run     bool
-	Results bool
-	Info    bool
+	Run     bool `json:"run" bson:"run"`
+	Results bool `json:"result" bson:"result"`
+	Info    bool `json:"info" bson:"info"`
+}
+
+// NewDevice create new device
+func NewDevice(device Device) (Device, error) {
+	if err := db.Session.Device().Insert(&device); err != nil {
+		return device, err
+	}
+	return device, nil
+}
+
+// ListDevices list devices
+func ListDevices() (DeviceList, error) {
+	var devices []Device
+	err := db.Session.Device().Find(nil).Sort("_id").All(&devices)
+	return DeviceList(devices), err
+}
+
+// DetailDevice detail device
+func DetailDevice(codename string) (Device, error) {
+	var device Device
+	err := db.Session.Device().FindId(codename).One(&device)
+	return device, err
+}
+
+// ModifyDevice modify device
+func ModifyDevice(codename string, device Device) error {
+	err := db.Session.Device().UpdateId(codename, device)
+	if err != nil {
+		return fmt.Errorf("error updating device: %s", err.Error())
+	}
+	return nil
+}
+
+// RemoveDevice remove device
+func RemoveDevice(name string) error {
+	err := db.Session.Device().RemoveId(name)
+	if err != nil {
+		return fmt.Errorf("error removing device: %s", err.Error())
+	}
+	return nil
 }
--- device/device_test.go
+++ device/device_test.go
@@ -1,1 +1,271 @@
 package device
+
+import (
+	"testing"
+
+	"github.com/globocom/config"
+	"launchpad.net/gocheck"
+
+	"github.com/wiliamsouza/apollo/db"
+)
+
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type S struct{}
+
+var _ = gocheck.Suite(&S{})
+
+func (s *S) SetUpSuite(c *gocheck.C) {
+	err := config.ReadConfigFile("../etc/apollod.conf")
+	c.Check(err, gocheck.IsNil)
+	config.Set("database:name", "apollo_customer_tests")
+	db.Connect()
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+	db.Session.DB.DropDatabase()
+}
+
+func (s *S) TestNewDevice(c *gocheck.C) {
+	codename := "a700"
+	permission := Permission{Run: true, Results: true, Info: true}
+	permissions := Permissions{Organization: permission, Team: permission}
+	d := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	device, err := NewDevice(d)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Device().RemoveId(codename)
+	var deviceDb Device
+	err = db.Session.Device().FindId(codename).One(&deviceDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(deviceDb, gocheck.DeepEquals, device)
+}
+
+func (s *S) TestListDevice(c *gocheck.C) {
+	codename1 := "a700"
+	codename2 := "a701"
+	permission := Permission{Run: true, Results: true, Info: true}
+	permissions := Permissions{Organization: permission, Team: permission}
+	d1 := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	d2 := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a701",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	device1, err := NewDevice(d1)
+	c.Assert(err, gocheck.IsNil)
+	device2, err := NewDevice(d2)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Device().RemoveId(codename1)
+	defer db.Session.Device().RemoveId(codename2)
+	deviceList := DeviceList{device1, device2}
+	deviceListDb, err := ListDevices()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(deviceListDb, gocheck.DeepEquals, deviceList)
+}
+
+func (s *S) TestDetailDevice(c *gocheck.C) {
+	codename := "a700"
+	permission := Permission{Run: true, Results: true, Info: true}
+	permissions := Permissions{Organization: permission, Team: permission}
+	d := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	device, err := NewDevice(d)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Device().RemoveId(codename)
+	deviceDb, err := DetailDevice(codename)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(deviceDb, gocheck.DeepEquals, device)
+}
+
+func (s *S) TestModifyDevice(c *gocheck.C) {
+	codename := "a700"
+	permission := Permission{Run: true, Results: true, Info: true}
+	permissions := Permissions{Organization: permission, Team: permission}
+	d1 := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	d2 := Device{
+		Permission:      permissions,
+		Name:            "Acer A701",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 4",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "2GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "64GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "15MP",
+		SecondaryCamera: "5MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	_, err := NewDevice(d1)
+	c.Assert(err, gocheck.IsNil)
+	defer db.Session.Device().RemoveId(codename)
+	err = ModifyDevice(codename, d2)
+	c.Assert(err, gocheck.IsNil)
+	var deviceDb Device
+	err = db.Session.Device().FindId(codename).One(&deviceDb)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(deviceDb, gocheck.DeepEquals, d2)
+}
+
+func (s *S) TestRemoveDevice(c *gocheck.C) {
+	permission := Permission{Run: true, Results: true, Info: true}
+	permissions := Permissions{Organization: permission, Team: permission}
+	d := Device{
+		Permission:      permissions,
+		Name:            "Acer A700",
+		Codename:        "a700",
+		Vendor:          "Acer",
+		Manufacturer:    "acer",
+		Type:            "tablet",
+		Platform:        "NVIDIA Tegra 3",
+		Cpu:             "1.3 GHz quad-core Cortex A9",
+		Gpu:             "416 MHz twelve-core Nvidia GeForce ULP",
+		Ram:             "1GB",
+		Weight:          "665 g (1.47 lb)",
+		Dimensions:      "259x175x11 mm (10.20x6.89x0.43 in)",
+		ScreenDimension: "257 mm (10.1 in)",
+		Resolution:      "1920x1200",
+		ScreenDensity:   "224 PPI",
+		InternalStorage: "32GB",
+		SdCard:          "up to 32 GB",
+		Bluetooth:       "yes",
+		WiFi:            "802.11 b/g/n",
+		MainCamera:      "5MP",
+		SecondaryCamera: "2MP",
+		Power:           "9800 mAh",
+		Peripherals:     "accelerometer, gyroscope, proximity sensor, digital compass, GPS, magnometer, microphone",
+	}
+	device, err := NewDevice(d)
+	c.Assert(err, gocheck.IsNil)
+	err = RemoveDevice(device.Codename)
+	c.Assert(err, gocheck.IsNil)
+	lenght, err := db.Session.Device().FindId(device.Codename).Count()
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(lenght, gocheck.Equals, 0)
+}
